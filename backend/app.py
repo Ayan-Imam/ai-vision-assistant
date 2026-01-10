d764b02c3bbfc7dc6ee8c5bb4b569120a6fcbd92
AYAN IMAM <ayanimam13@gmail.com>
2026-01-10 15:11:06 +0530

    Phase 3: Image upload from frontend to backen

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
 @app.route("/")
 def home():
-    return "Backend is running"
+    return "Backend is running!"
+
+@app.route("/upload", methods=["POST"])
+def upload_image():
+    if "image" not in request.files:
+        return jsonify({"error": "No image received"}), 400
+
+    image = request.files["image"]
+    return jsonify({
+        "message": "Image received successfully",
+        "filename": image.filename
+    })
+
 if __name__ == "__main__":
     app.run(debug=True)
